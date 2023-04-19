1346dd502a8b07c259a9f35415c60b405f99f721
Cherry Leung <yi38leung@gmail.com>
2023-04-19 11:05:57 +0200

    adding api to spot R

--- routes/spot.routes.js
+++ routes/spot.routes.js
@@ -6,7 +6,7 @@ const { isAuthenticated } = require("../middleware/jwt.middleware.js");
 const Spot = require("../models/Spot.model");
 
 // POST - CREATE a spot
-router.post("/", isAuthenticated, (req, res, next) => {
+router.post("/api", isAuthenticated, (req, res, next) => {
   const { name, description, category, image } = req.body;
   const userId = req.payload._id;
 
@@ -16,14 +16,14 @@ router.post("/", isAuthenticated, (req, res, next) => {
 });
 
 // GET - DISPLAY all spots
-router.get("/", (req, res, next) => {
+router.get("/api", (req, res, next) => {
   Spot.find()
     .then((allSpots) => res.json(allSpots))
     .catch((error) => res.json(error));
 });
 
 // GET - DISPLAY a spot
-router.get("/:spotId", isAuthenticated, (req, res, next) => {
+router.get("/api:spotId", isAuthenticated, (req, res, next) => {
   const { spotId } = req.params;
 
   if (!mongoose.Types.ObjectId.isValid(spotId)) {
